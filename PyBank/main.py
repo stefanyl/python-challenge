3b6e6410be8d3b1a624604b61610bbf84cf15802
Stefany Lima <stefanylima@Stefanys-Air.attlocal.net>
2021-07-16 02:13:41 -0500

    added for loop and print statements

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,44 +1,49 @@
 #Import the os module
 import os
 
-#Module for reading in CSV file
+#Import module for reading in CSV file and set path.
 import csv
-
 csvpath = os.path.join('Resources', 'budget_data.csv')
-#print(csvpath)
-
-
-#Read in the csv and stores the header row
-with open(csvpath) as budget_data:
-    csvreader = csv.reader(budget_data, delimiter=',')
-    #print(csvreader)
-
-    csv_header = next(csvreader)
-    #print(f"Header Row: {csv_header}")
-
-    for row in csvreader:
-       #print(row)
-
-print("Financial Analysis")
-print("---------------------------")
 
 #set variables for Financial Analysis
-        total_months = []
-        total_profit = []
-        profit_change = []
+total_months = []
+total_profit = []
+profit_change = []
 
-        #Calculate the total number of months included in the dataset. Print.
+#Read in the CSV and store the header row.
+with open(csvpath, encoding='utf-8-sig') as budget_data:
+    csvreader = csv.reader(budget_data, delimiter=',')
+    print(csvreader)
+
+    csv_header = next(csvreader)
+
+    #for loop to iterate through the rows
+    for row in csvreader:
+
+        #Calculate the total number of months included in the dataset. 
         total_months.append(row[0])
-        print(f"Total Months: {len(total_months)}")
         
         #calculate the net total amount of "Profit/Losses" over the entire period
         total_profit.append(int(row[1]))
-        print(f"Total: ${sum(total_profit)}")
-        
-        #calculate the changes in "Profit/Losses" over the entire period. Iterate.Then, find average of those changes.
-        for i = 2 to 87
-            profit_change.append(total_profit[i+1]-total_profit[1])
-#find the greatest increase in profits over the entires period
-max_increase_value = max(monthly_profit_change)
-max_decrease_value = min(monthly_profit_change)
-#find the greatest decrease in profits over the entire period
+      
+    #calculate the changes in "Profit/Losses" over the entire period. 
+    #Iterate through profits to find average of those changes.
+    for i = range(len(total_profit)-1):
+        profit_change.append(total_profit[i+1]-total_profit[1])
+
+        #find the greatest increase in profits over the entire period
+        max_increase = max(profit_change)
+        max_increase_period = profit_change.index(max(profit_change)) + 1
+
+        #find the greatest decrease in profits over the entire period
+        max_decrease = min(profit_change)
+        max_decrease_period = profit_change.index(min(profit_change)) + 1 
+
+#print statement and export text file to analysis folder
+print("Financial Analysis")  
+print("--------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(profit_change)/len(profit_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_period]} (${(str(max_increase))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_period]} (${(str(max_decrease))})")
