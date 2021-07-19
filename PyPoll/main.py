fbba6dad12b2e380b0ba76a4c26d5d4efa974383
Stefany Lima <stefanylima@Stefanys-Air.attlocal.net>
2021-07-19 18:07:56 -0500

    results display but need to reduce demimal spaces, winner name to pull, removed votingresults{}

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,10 +12,6 @@ correy_votes = 0
 li_votes = 0
 otooley_votes = 0
 
-khan_percentage = 0
-correy_percentage = 0
-li_percentage = 0
-otooley_percentage = 0
 
 #Open csv and skip the header.
 with open(csvpath, newline="", encoding="utf-8") as election_data:
@@ -26,45 +22,55 @@ with open(csvpath, newline="", encoding="utf-8") as election_data:
     #Find the total number of votes cast. 
     for row in csvreader:
         total_votes += 1
-    print(f"Total Votes: {total_votes}")
 
-#List the candidates who received votes - Khan, Correy, Li, Otooley. 
-#Find the total number of votes each candidate won.
+        #List the candidates who received votes - Khan, Correy, Li, Otooley. 
+        #Find the total number of votes each candidate won.
         if row[2] == "Khan": 
             khan_votes += 1
-        elif    row[2] == "Correy":
+        elif row[2] == "Correy":
             correy_votes += 1
         elif row[2] == "Li": 
             li_votes += 1
-        else row[2] == "O'Tooley":
+        elif row[2] == "O'Tooley":
             otooley_votes += 1 
 
 #The winner of the election based on popular vote - need to fix 
 #winner = max.total_votes
     election_winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
-
-        if election_winner == khan_votes:
-            winner_name = "Khan"
-        elif election_winner == correy_votes:
-            winner_name = "Correy"
-        elif election_winner == li_votes:
-            winner_name = "Li"
-        else election_winner == otooley_votes:
-            winner_name = "O'Tooley"
+    if election_winner == khan_votes:
+        winner_name = "Khan"
+    elif election_winner == correy_votes:
+        winner_name = "Correy"
+    elif election_winner == li_votes:
+        winner_name = "Li"
+    elif election_winner == otooley_votes:
+        winner_name = "O'Tooley"
 
 #Find the percentage of votes each candidate won.
-    khan_percentage = (khan_votes/total_votes) *100
-    correy_percentage = (correy_votes/total_votes) * 100
-    li_percentage = (li_votes/total_votes)* 100
-    otooley_percentage = (otooley_votes/total_votes) * 100
+khan_percentage = (khan_votes/total_votes) *100
+correy_percentage = (correy_votes/total_votes) * 100
+li_percentage = (li_votes/total_votes)* 100
+otooley_percentage = (otooley_votes/total_votes) * 100
+
+#Make a dictionary with results
+#votingresults = {
+    #"Khan": khan_percentage, (khan_votes),
+    #"Correy": correy_percentage, (correy_votes),
+    #"Li": li_percentage, (li_votes),
+    #"O'Tooley": otooley_percentage, otooley_votes),
+    #}
     
 #Print your analysis 
-votingresults = {
-    "Khan":[khan_percentage, khan_votes],
-    "Correy": [correy_percentage, correy_votes],
-    "Li": [li_percentage, li_votes],
-    "O'Tooley": [otooley_percentage, otooley_votes]
-}
-print(f"voting results": {votingresults})
+print(f"Election Results")
+print(f"-----------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"-----------------------------")
+print(f"Khan: {khan_percentage}, ({khan_votes})")
+print(f"Correy: {correy_percentage}, ({correy_votes})")
+print(f"Li: {li_percentage}, ({li_votes})")
+print(f"O'Tooley: {otooley_percentage}, ({otooley_votes})")
+print(f"-----------------------------")
+print(f"Winner: {election_winner}")
+print(f"-----------------------------")
 
 #Export analysis as a text file
